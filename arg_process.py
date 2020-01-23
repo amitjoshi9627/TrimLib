b6c18e0bb3ecd5e1a2db4255a496abdaf524f5f4
amitjoshi9627 <amitjoshi9627@gmail.com>
2020-01-23 17:44:08 +0530

    Minor Change

--- arg_process.py
+++ arg_process.py
@@ -1,18 +1,20 @@
 import argparse
 
+
 def print_error(error_msg):
     if "unrecognized arguments" in error_msg:
         print("Invalid Arguments. Please see help [-h] or [--help]")
     else:
         pass
 
+
 def parse_argument():
     parser = argparse.ArgumentParser(
-        prog="trimlib.py", description="Remove unused imports")
-    
+        prog="trimport.py", description="Remove unused imports")
+
     parser.add_argument("-l", '--location', dest='file_location',
-                        help="Location of Target file", required= True)
-    
+                        help="Location of Target file", required=True)
+
     try:
         parser.error = print_error
         args = parser.parse_args()
@@ -22,4 +24,3 @@ def parse_argument():
             return args.file_location
     except:
         print("Please provide the file location.")
-    
